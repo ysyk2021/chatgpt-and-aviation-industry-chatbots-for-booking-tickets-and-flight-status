888dac0d858f53ecedf8042cb8ebf75d7bd89b17
ysykzheng <1020680508@qq.com>
2023-09-07 11:52:42 +0800

    init

--- book.sh
+++ book.sh
@@ -1,9 +1,11 @@
 npm install
 npx honkit epub ./ chatgpt-and-aviation-industry-chatbots-for-booking-tickets-and-flight-status.epub
 
-ebook-convert chatgpt-and-aviation-industry-chatbots-for-booking-tickets-and-flight-status.epub chatgpt-and-aviation-industry-chatbots-for-booking-tickets-and-flight-status.pdf --no-chapters-in-toc --chapter-mark=none --subset-embedded-fonts  --pdf-page-numbers --disable-markup-chapter-headings --paper-size a5 --pdf-page-margin-bottom 36 --pdf-page-margin-left 36 --pdf-page-margin-right 36 --pdf-page-margin-top 36 --extra-css page_styles.css
+ebook-convert chatgpt-and-aviation-industry-chatbots-for-booking-tickets-and-flight-status.epub chatgpt-and-aviation-industry-chatbots-for-booking-tickets-and-flight-status.pdf --no-chapters-in-toc --chapter-mark=none --pdf-page-numbers --disable-markup-chapter-headings --paper-size a5 --pdf-page-margin-bottom 36 --pdf-page-margin-left 36 --pdf-page-margin-right 36 --pdf-page-margin-top 36 --extra-css page_styles.css
+
+ebook-convert chatgpt-and-aviation-industry-chatbots-for-booking-tickets-and-flight-status.epub chatgpt-and-aviation-industry-chatbots-for-booking-tickets-and-flight-status.mobi
+
+ebook-convert chatgpt-and-aviation-industry-chatbots-for-booking-tickets-and-flight-status.epub chatgpt-and-aviation-industry-chatbots-for-booking-tickets-and-flight-status.azw3
 
 ebook-convert chatgpt-and-aviation-industry-chatbots-for-booking-tickets-and-flight-status.epub chatgpt-and-aviation-industry-chatbots-for-booking-tickets-and-flight-status.docx --no-chapters-in-toc --chapter-mark=none --disable-markup-chapter-headings --docx-no-toc --docx-no-cover --docx-page-size=a5 --extra-css page_styles.css
 
-# brew install pdftk-java
-java -jar "/usr/local/Cellar/pdftk-java/3.3.3/libexec/pdftk-all.jar" chatgpt-and-aviation-industry-chatbots-for-booking-tickets-and-flight-status.pdf cat 2-end output chatgpt-and-aviation-industry-chatbots-for-booking-tickets-and-flight-status-FINAL.pdf
---
+++ src/other-books.md
@@ -0,0 +1,14 @@
+
+Want get more our free books? click here: [https://zhengpublishing.com/subscribe/](https://zhengpublishing.com/subscribe/)
+
+- [2039](ChatGPT and Social Good: Making a Positive Impact with Conversational AI)
+- [2477](The Path to Mastery: How Lifelong Learning Can Help You Achieve Your Goals)
+- [2215](Using AI to Take Small Businesses to the Next Level: Techniques and Strategies for Enhanced Performance)
+- [2330](How to Promote Workplace Health and Safety for Enhanced Well-Being and Performance)
+- [2444](The Productivity Challenge: A 30-Day Program for Boosting Efficiency)
+- [2312](The Personal Branding Mindset for Professionals: How to Think Like a Brand)
+- [2151](The Ethics of AI: Navigating Complex Challenges and Opportunities)
+- [2033](ChatGPT and Supply Chain Management: Automating Logistics and Improving Delivery Times)
+- [42](How to Create a High-Performance Workplace Team that Achieves Outstanding Results with Effective Collaboration and Performance Strategies)
+- [2146](The Intersection of AI and Writing: A Guide to Improving Your Writing Skills with Machine Learning)
+
